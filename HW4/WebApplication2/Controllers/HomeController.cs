eee49f5504912e91643d271453dee382536ebd64
Rochelle Simpson <rsimpson15@wou.mail.edu>
2017-10-30 19:15:12 -0700

    Uploaded completion of Page2.

--- HW4/WebApplication2/Controllers/HomeController.cs
+++ HW4/WebApplication2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace WebApplication2.Controllers
 {
@@ -34,12 +35,144 @@ namespace WebApplication2.Controllers
 
         public ActionResult Page1()
         {
+            string word;
+            string lang;
+
+            if (!string.IsNullOrEmpty(Request.Form["feildOne"]) && !string.IsNullOrEmpty(Request.Form["feildTwo"]))
+            {
+                word = Request.Form["feildOne"];
+                lang = Request.Form["feildTwo"];
+                int size = word.Length - 2;
+
+                if (lang == "P" || lang == "p")
+                {
+                    string flavor = "ay";
+                    if (word.Length == 1)
+                    {
+                        ViewBag.word = word + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                    else if (word.Length == 2)
+                    {
+                        string first = word.Substring(0, 0);
+                        string last = word.Substring(1, 1);
+                        ViewBag.word = last + first + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                    else if (word.Length == 3)
+                    {
+                        string start = word.Substring(0, 1);
+                        string end = word.Substring(2, 2);
+                        ViewBag.word = end + start + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                    else
+                    {
+                        string start = word.Substring(0, 1).ToLower();
+                        string bulk = word.Substring(2, size);
+                        ViewBag.word = bulk + start + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                }
+                else if (lang == "E" || lang == "e")
+                {
+                    string end = word.Substring(word.Length - 3, size - 2);
+                    string bulk = word.Substring(0, size - 4);
+                    ViewBag.word = end + bulk;
+                    ViewData["word"] = ViewBag.word;
+                    return View();
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Please enter E for English or P for Piglatin.";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please enter a value for both feilds.";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Page2()
+        {
+            ViewBag.RequestMethod = "GET";
             return View();
         }
 
-        public ActionResult Page2()
+        [HttpPost]
+        public ActionResult Page2(string feildOne, string feildTwo)
         {
-            return View();
+            ViewBag.RequestMethod = "POST";
+
+            string word;
+            string lang;
+
+            if (!string.IsNullOrEmpty(feildOne) && !string.IsNullOrEmpty(feildTwo))
+            {
+                word = feildOne;
+                lang = feildTwo;
+                int size = word.Length - 2;
+
+                if (lang == "P" || lang == "p")
+                {
+                    string flavor = "ay";
+                    if (word.Length == 1)
+                    {
+                        ViewBag.word = word + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                    else if (word.Length == 2)
+                    {
+                        string first = word.Substring(0, 0);
+                        string last = word.Substring(1, 1);
+                        ViewBag.word = last + first + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                    else if (word.Length == 3)
+                    {
+                        string start = word.Substring(0, 1);
+                        string end = word.Substring(2, 2);
+                        ViewBag.word = end + start + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                    else
+                    {
+                        string start = word.Substring(0, 1).ToLower();
+                        string bulk = word.Substring(2, size);
+                        ViewBag.word = bulk + start + flavor;
+                        ViewData["word"] = ViewBag.word;
+                        return View();
+                    }
+                }
+                else if (lang == "E" || lang == "e")
+                {
+                    string end = word.Substring(word.Length - 3, size - 2);
+                    string bulk = word.Substring(0, size - 4);
+                    ViewBag.word = end + bulk;
+                    ViewData["word"] = ViewBag.word;
+                    return View();
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Please enter E for English or P for Piglatin.";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please enter a value for both feilds.";
+                return View();
+            }
         }
 
         public ActionResult Page3()
