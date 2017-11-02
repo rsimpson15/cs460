64209a8366e3ee7da707be75bb7e2cc3e5cbadd9
Rochelle Simpson <rsimpson15@wou.mail.edu>
2017-11-02 07:54:53 -0700

    Uploading Page3, Loan Caclulator still doesn't return value.

--- HW4/WebApplication2/Controllers/HomeController.cs
+++ HW4/WebApplication2/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Diagnostics;
+using System.Net;
 
 namespace WebApplication2.Controllers
 {
@@ -35,26 +36,33 @@ namespace WebApplication2.Controllers
 
         public ActionResult Page1()
         {
+            //Variables
             string word;
             string lang;
 
+            //Test if our fields now hold values instead of nulls
             if (!string.IsNullOrEmpty(Request.Form["feildOne"]) && !string.IsNullOrEmpty(Request.Form["feildTwo"]))
             {
+                //Input the field data to our vairables
                 word = Request.Form["feildOne"];
                 lang = Request.Form["feildTwo"];
+                //Create a reusable size variable
                 int size = word.Length - 2;
 
+                //If the langauge is Piglatin
                 if (lang == "P" || lang == "p")
                 {
                     string flavor = "ay";
                     if (word.Length == 1)
                     {
+                        //For one letter words just attach "ay" to it
                         ViewBag.word = word + flavor;
                         ViewData["word"] = ViewBag.word;
                         return View();
                     }
                     else if (word.Length == 2)
                     {
+                        //For two letter words swap the letter positions and attach "ay"
                         string first = word.Substring(0, 0);
                         string last = word.Substring(1, 1);
                         ViewBag.word = last + first + flavor;
@@ -63,6 +71,7 @@ namespace WebApplication2.Controllers
                     }
                     else if (word.Length == 3)
                     {
+                        //For three letter words, take the first two, move them to the rear and attach "ay"
                         string start = word.Substring(0, 1);
                         string end = word.Substring(2, 2);
                         ViewBag.word = end + start + flavor;
@@ -71,6 +80,7 @@ namespace WebApplication2.Controllers
                     }
                     else
                     {
+                        //For all other cases,take the front two letter and move them to the rear, then attach "ay"
                         string start = word.Substring(0, 1).ToLower();
                         string bulk = word.Substring(2, size);
                         ViewBag.word = bulk + start + flavor;
@@ -78,8 +88,10 @@ namespace WebApplication2.Controllers
                         return View();
                     }
                 }
+                //If language is English
                 else if (lang == "E" || lang == "e")
                 {
+                    //Take the rear two letters and move them to the front, remove "ay"
                     string end = word.Substring(word.Length - 3, size - 2);
                     string bulk = word.Substring(0, size - 4);
                     ViewBag.word = end + bulk;
@@ -88,12 +100,14 @@ namespace WebApplication2.Controllers
                 }
                 else
                 {
+                    //If langauge is not English or Piglatin throw an error message
                     ViewBag.ErrorMessage = "Please enter E for English or P for Piglatin.";
                     return View();
                 }
             }
             else
             {
+                //If one of the feilds is null, throw an error message
                 ViewBag.ErrorMessage = "Please enter a value for both feilds.";
                 return View();
             }
@@ -102,6 +116,7 @@ namespace WebApplication2.Controllers
         [HttpGet]
         public ActionResult Page2()
         {
+            //Get Method
             ViewBag.RequestMethod = "GET";
             return View();
         }
@@ -109,28 +124,36 @@ namespace WebApplication2.Controllers
         [HttpPost]
         public ActionResult Page2(string feildOne, string feildTwo)
         {
+            //Post Method
             ViewBag.RequestMethod = "POST";
 
+            //Variables
             string word;
             string lang;
 
+            //Check if the fields are no longer null
             if (!string.IsNullOrEmpty(feildOne) && !string.IsNullOrEmpty(feildTwo))
             {
+                //If so, put the field data into variables
                 word = feildOne;
                 lang = feildTwo;
+                //Create a size variable
                 int size = word.Length - 2;
 
+                //If the langauge is Piglatin
                 if (lang == "P" || lang == "p")
                 {
                     string flavor = "ay";
                     if (word.Length == 1)
                     {
+                        //For one letter words just attach "ay" to it
                         ViewBag.word = word + flavor;
                         ViewData["word"] = ViewBag.word;
                         return View();
                     }
                     else if (word.Length == 2)
                     {
+                        //For two letter words swap the letter positions and attach "ay"
                         string first = word.Substring(0, 0);
                         string last = word.Substring(1, 1);
                         ViewBag.word = last + first + flavor;
@@ -139,6 +162,7 @@ namespace WebApplication2.Controllers
                     }
                     else if (word.Length == 3)
                     {
+                        //For three letter words, take the first two, move them to the rear and attach "ay"
                         string start = word.Substring(0, 1);
                         string end = word.Substring(2, 2);
                         ViewBag.word = end + start + flavor;
@@ -147,6 +171,7 @@ namespace WebApplication2.Controllers
                     }
                     else
                     {
+                        //For all other cases,take the front two letter and move them to the rear, then attach "ay"
                         string start = word.Substring(0, 1).ToLower();
                         string bulk = word.Substring(2, size);
                         ViewBag.word = bulk + start + flavor;
@@ -154,8 +179,10 @@ namespace WebApplication2.Controllers
                         return View();
                     }
                 }
+                //If language is English
                 else if (lang == "E" || lang == "e")
                 {
+                    //Take the rear two letters and move them to the front, remove "ay"
                     string end = word.Substring(word.Length - 3, size - 2);
                     string bulk = word.Substring(0, size - 4);
                     ViewBag.word = end + bulk;
@@ -164,20 +191,54 @@ namespace WebApplication2.Controllers
                 }
                 else
                 {
+                    //If langauge is not English or Piglatin throw an error message
                     ViewBag.ErrorMessage = "Please enter E for English or P for Piglatin.";
                     return View();
                 }
             }
             else
             {
+                //If one of the feilds is null, throw an error message
                 ViewBag.ErrorMessage = "Please enter a value for both feilds.";
                 return View();
             }
         }
 
+        [HttpGet]
         public ActionResult Page3()
         {
+            //Get Method
+            ViewBag.RequestMethod = "GET";
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Page3(decimal? amount, decimal? interest, decimal? term)
+        {
+            //Post Method
+            ViewBag.RequestMethod = "POST";
+
+            //Variables
+            decimal principle, rate, time, total;
+
+            //Test if our fields are null
+            if (amount != null && interest != null && term != null)
+            {
+                //Double check the format by converting
+                principle = Convert.ToDecimal(amount);
+                rate = Convert.ToDecimal(interest);
+                time = Convert.ToDecimal(term);
+
+                //Perform math calculation
+                total = (principle * time * rate) / 100;
+
+                ViewBag.total = total;
+                return View();
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
