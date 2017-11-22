6722d795682ed793a4b2a953c25c2e7ee8c6e436
Rochelle Simpson <rsimpson15@wou.mail.edu>
2017-11-21 23:47:24 -0800

    Completed Project

---
+++ homework6/homework6/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using homework6.Models;
+using System.Globalization;
+using homework6.Models.ViewModel;
+
+namespace homework6.Controllers
+{
+    public class ProductController : Controller
+    {
+        private AdventureWorksDbContext db = new AdventureWorksDbContext();
+
+        // GET: Product
+        public ActionResult Index(int? id, int?subID)
+        {
+            ProductCatandSubCatVM vm = new ProductCatandSubCatVM() {
+                ProductCategoriesName = db.ProductCategories,
+                ProductSubcategoriesName = db.ProductSubcategories
+            };
+
+            if(id != null && db.ProductCategories.Find(id) != null)
+            {
+                ViewBag.ID = id;
+            }
+
+            return View(vm);
+        }
+
+        public ActionResult Browse(int? id){
+            ViewBag.SubID = id;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCatandSubCatVM vm = new ProductCatandSubCatVM()
+            {
+                ProductCategoriesName = db.ProductCategories,
+                ProductSubcategoriesName = db.ProductSubcategories.Where(p => p.ProductCategory.ProductCategoryID == id),
+                Product = db.ProductSubcategories.ToList()[id.Value - 1].Products.ToList()
+            };
+            ViewBag.Count = vm.Product.Count() / 10;
+
+            return View(vm);
+         }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ProductCatandSubCatVM vm = new ProductCatandSubCatVM()
+            {
+                ProductCategoriesName = db.ProductCategories,
+                ProductSubcategoriesName = db.ProductSubcategories,
+                Product = db.Products.ToList(),
+                PReveiw = db.ProductReviews.ToList()
+            };
+
+            ViewBag.pName = db.Products.Where(p => p.ProductID == id).Select(p => p.Name).FirstOrDefault().ToString();
+            ViewBag.pWeight = db.Products.Where(p => p.ProductID == id).Select(p => p.Weight).FirstOrDefault().ToString();
+            ViewBag.pSize = db.Products.Where(p => p.ProductID == id).Select(p => p.Size).FirstOrDefault().ToString();
+            ViewBag.pCost = db.Products.Where(p => p.ProductID == id).Select(p => p.ListPrice).FirstOrDefault().ToString(); ;
+            ViewBag.pColor = db.Products.Where(p => p.ProductID == id).Select(p => p.Color).FirstOrDefault().ToString();
+            ViewBag.pPhoto = db.ProductProductPhotoes.Where(p => p.ProductID == id).Select(c => c.ProductPhoto.LargePhoto).FirstOrDefault();
+
+            return View(vm);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ homework6/homework6/Models/ViewModel/ProductCatandSubCatVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using homework6.Models;
+
+namespace homework6.Models.ViewModel
+{
+    public class ProductCatandSubCatVM
+    {
+
+        public IEnumerable<ProductCategory> ProductCategoriesName { get; set; }
+        public IEnumerable<ProductSubcategory> ProductSubcategoriesName { get; set; }
+        public IEnumerable<Product> Product { get; set; }
+        public IEnumerable<Product> Product2 { get; set; }
+        public IEnumerable<ProductModel> PModel { get; set; }
+        public IEnumerable<ProductListPriceHistory> Price { get; set; }
+        public IEnumerable<ProductReview> PReveiw { get; set; }
+    }
+}
