5954d353418fc0476e9f838b703b2d020fe8dda3
Rochelle Simpson <rsimpson15@wou.mail.edu>
2017-11-29 19:59:50 -0800

    Javascript now returns an error, so thats... something.

--- HW8/HW8/Scripts/AjaxRequest.js
+++ HW8/HW8/Scripts/AjaxRequest.js
@@ -1,26 +1,20 @@
-﻿
-$(document).ready(function () {
+﻿function callFunction(genre) {
+    $("#list").empty();//Clear old data before ajax
 
-    function callFunction(genre) {
-        $("#list").empty();//Clear old data before ajax
-        console.log('clicked');
+    $.ajax({
+        url: '@URL.Action("GetGenre")',
+        type: 'POST',
+        data: { 'Name': 'Tesselation' },
+        success: function (data) {
+            console.log('success?');
+            var table = $("#list");
+            returnData.arr.forEach(data, function (elem) {
+                $('#list').append(item);
+            });
+        },
+        error: function () {
+            alert('There was an error.');
+        }
+    });
 
-        $.ajax({
-            url: '@URL.Action("GetGenre")',
-            type: 'POST',
-            data: {'Name': 'Tesselation' },
-            success: function (data) {
-                console.log('success?');
-                var table = $("#list");
-                returnData.arr.forEach(data, function (elem) {
-                    $('#list').append(item);
-                });
-            },
-            error: function () {
-                alert('There was an error.');
-            }
-        });
-
-    };
-
-});
+};
