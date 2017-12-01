b4bd6454b643b7d101f6607dc6117a85db973897
Rochelle Simpson <rsimpson15@wou.mail.edu>
2017-12-01 11:09:15 -0800

    Updated MarkUP and MarkDOWN files.

--- HW8/HW8/App_Data/markUp.sql
+++ HW8/HW8/App_Data/markUp.sql
@@ -1,5 +1,45 @@
 ﻿/*MARK UP for DATABASE*/
 
+USE [C:\USERS\ROCHELLE SIMPSON\DOCUMENTS\CS460\HW8\HW8\APP_DATA\ARTGALLERYDB.MDF];  
+
+CREATE TABLE [dbo].[Artist] (
+    [ArtistID]     INT            NOT NULL,
+    [Name]         NVARCHAR (150) NOT NULL,
+    [Birthdate]    DATETIME       NOT NULL,
+    [BirthCountry] NVARCHAR (150) NOT NULL,
+    [BirthCity]    NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ArtistID] ASC)
+);
+
+CREATE TABLE [dbo].[Artwork] (
+    [ArtworkID] INT            NOT NULL,
+    [ArtistID]  INT            NOT NULL,
+    [Title]     NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ArtworkID] ASC),
+    FOREIGN KEY ([ArtistID]) REFERENCES [dbo].[Artist] ([ArtistID])
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
+
+CREATE TABLE [dbo].[Genre] (
+	[GenreID] INT NOT NULL,
+    [Name]    NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([GenreID] ASC)
+);
+
+CREATE TABLE [dbo].[Classification] (
+    [ClassificationID] INT NOT NULL,
+    [ArtworkID]        INT NOT NULL,
+    [GenreID]        NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ClassificationID] ASC),
+    FOREIGN KEY ([ArtworkID]) REFERENCES [dbo].[Artwork] ([ArtworkID])
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+    FOREIGN KEY ([GenreID]) REFERENCES [dbo].[Genre] ([GenreID])
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
 INSERT INTO Artist(ArtistID, Name, Birthdate, BirthCountry, BirthCity)
 VALUES
 	(00, 'M.C. Esher', 07/17/1898, 'Netherlands', 'Leeuwarden'),
